fb859527405d0c07a4e8a0e517eb233fa3f9cf12
rudi <sogat@meuser.it>
2023-08-22 12:28:16 +0200

    added theme switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from 'next/font/google'
 import { Toaster } from '@/components/ui/toaster'
 import TitleBar from '@/components/TitleBar'
 import { Badge } from '@/components/ui/badge'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '300'] })
 
@@ -14,19 +15,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html className="dark" lang="en">
+    <html lang="en">
       <body className={`${poppins.className}`}>
-        <TitleBar />
-        <main className="flex flex-col gap-10 justify-between items-center h-[100vh] py-12 px-10">
-          <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            NitroSnap
-            <Badge className="ml-2 font-normal tracking-normal" variant={'secondary'}>
-              Beta
-            </Badge>
-          </h1>
-          {children}
-        </main>
-        <Toaster />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <TitleBar />
+          <main className="flex flex-col gap-10 justify-between items-center h-[100vh] py-12 px-10">
+            <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+              NitroSnap
+              <Badge className="ml-2 font-normal tracking-normal" variant={'secondary'}>
+                Beta
+              </Badge>
+            </h1>
+            {children}
+          </main>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   )
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,15 +1,29 @@
 'use client'
 import AppVersion from '@/components/AppVersion'
+import { ThemeToggle } from '@/components/ThemeToggle'
 import { Button } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
-import { ChevronLeft } from 'lucide-react'
+import { ChevronLeft, SunIcon } from 'lucide-react'
 import Link from 'next/link'
 import { Fragment } from 'react'
 
 export default function Home() {
   return (
     <Fragment>
-      <ScrollArea className="w-[70vw] grow"></ScrollArea>
+      <ScrollArea className="w-[70vw] grow">
+        <div className="flex flex-col gap-2.5 ">
+          <div className=" flex items-center space-x-4 rounded-md border p-4">
+            <SunIcon />
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">Theme</p>
+              <p className="text-sm text-muted-foreground">
+                Select your desired theme mode
+              </p>
+            </div>
+            <ThemeToggle />
+          </div>
+        </div>
+      </ScrollArea>
       <Button asChild size={'lg'} variant={'secondary'}>
         <Link href="/">
           <ChevronLeft className="mr-2 h-4 w-4" /> Back
